db1d1870106a0c8bb5a1c8b7aec37f8d69d1de2c
Yidiiiz <876yidi@gmail.com>
2025-04-27 02:24:23 -0400

    furnish

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ var sideOpen = true;
 function toggleNav() {
     navOpen = !navOpen;
     if (navOpen){
-        document.getElementById("infoPanel").style.left = "0%";
-        document.getElementById("scrollPanel").style.left = "-300px";
-    } else {
         document.getElementById("scrollPanel").style.left = "0%";
         document.getElementById("infoPanel").style.left = "-300px";
+    } else {
+        document.getElementById("infoPanel").style.left = "0%";
+        document.getElementById("scrollPanel").style.left = "-300px";
     }
 }
 
@@ -19,12 +19,12 @@ function toggleSide() {
         document.getElementById("sidePanel").style.left = "0";
         document.getElementById("mainPanel").style.left = "300px";
         
-        document.getElementById("hideBtn").style.left = "-30px";
+        document.getElementById("hideBtn").style.left = "-24px";
         
         setTimeout(() => {
             document.getElementById("hideImg").src = "assets/website/Hide.png";
-            document.getElementById("hideBtn").style.left = "24px";
-        }, 600);
+            document.getElementById("hideBtn").style.left = "324px";
+        }, 200);
     } else {
         document.getElementById("sidePanel").style.left = "-300px";
         document.getElementById("mainPanel").style.left = "0";
@@ -34,6 +34,6 @@ function toggleSide() {
         setTimeout(() => {
             document.getElementById("hideImg").src = "assets/website/Show.png";
             document.getElementById("hideBtn").style.left = "24px";
-        }, 600);
+        }, 400);
     }
 }
